df6d26559c059aa22d72bd13fcae0312c1bb1712
esdras2508 <esdras2508@gmail.com>
2015-09-17 00:20:57 -0600

    Sincronizacion 09/17/2015 12:19

---
+++ src/java/com/serverProject/arbolUsr/nodoAVL.java
@@ -0,0 +1,41 @@
+package com.proyectoPruebas.principal;
+
+public class nodoAVL {
+	int balance;
+	int rotacion;
+	nodoAVL izquierda;
+	nodoAVL derecha;
+	
+	String correo, pass;
+	
+	public nodoAVL() {
+		balance =0;
+		rotacion = 0;
+		izquierda = null;
+		derecha = null;
+		correo = "";
+		pass = "";
+	}
+	
+	public void Cambiar_Balance(int balance){
+		this.balance = balance;
+	}
+	
+	public void Cambiar_Izquierda(nodoAVL izquierda){
+		this.izquierda = izquierda;
+	}
+	
+	public void Cambiar_Derecha(nodoAVL derecha){
+		this.derecha = derecha;
+	}
+	
+	public void Cambiar_Correo(String correo){
+		this.correo = correo;
+	}
+	
+	public void Cambiar_Pass(String pass){
+		this.pass = pass;
+	}
+	
+	
+}
